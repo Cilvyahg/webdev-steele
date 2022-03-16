cde277b495f92c86067d11818554fc6919e59435
Cilvya <cilvya.hg@gmail.com>
2022-03-16 16:55:59 +0100

    object literals

--- JS_array/array_cheatsheet.js
+++ JS_array/array_cheatsheet.js
@@ -14,3 +14,29 @@
 //unshift(val) - adds value to START of an array
 //shift() - removes and returns first element in an array
 
+const gameBoard = [
+    ['X', 'O', 'X'],
+    ['O', null, 'X'],
+    ['O', 'O', 'X']
+];
+
+
+//OBJECT LITERALS
+
+const kitchenSink = {
+    faveNum: 92319023,
+    isFunny: true,
+    colors: ['red', 'pink']
+}
+
+// how to get acces to object literals. Every key will turn into strings. even if its a boolean, null or etc.except for symbols
+
+
+kitchenSink.faveNum
+
+const years = {
+    1999: 'Good',
+    2020: 'Bad'
+}
+
+years["1999"]
