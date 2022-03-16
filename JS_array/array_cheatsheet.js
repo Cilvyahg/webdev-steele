d108a0b9fa44eb624289c576b315d5b335211749
Cilvya <cilvya.hg@gmail.com>
2022-03-16 13:24:37 +0100

    array

--- JS_array/array_cheatsheet.js
+++ JS_array/array_cheatsheet.js
@@ -1,11 +1,11 @@
-// Making an array:
-const colors = ["red", "orange", "yellow"];
+// // Making an array:
+// const colors = ["red", "orange", "yellow"];
 
-// Arrays are indexed like strings:
-colors[0]; // "red"
+// // Arrays are indexed like strings:
+// colors[0]; // "red"
 
-// They have a length:
-colors.length; //3
+// // They have a length:
+// colors.length; //3
 
 // Important array methods:
 //push(value) - adds value to the END of an array
@@ -13,3 +13,4 @@ colors.length; //3
 
 //unshift(val) - adds value to START of an array
 //shift() - removes and returns first element in an array
+
